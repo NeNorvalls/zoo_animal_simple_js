324c6d3be930e54758ded72d15d2e0ef40e9d1f1
NeNorvalls <honeygeeh27@gmail.com>
2024-12-11 11:02:48 +0100

    adding html, css and js

---
+++ script.js
@@ -0,0 +1,10 @@
+let counter = 3;
+
+function upvote() {
+  counter++;
+  document.getElementById("votes").innerText = counter + " Votes";
+}
+function downvote() {
+  counter--;
+  document.getElementById("votes").innerText = counter + " Votes";
+}
